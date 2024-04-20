15758eb8c85a95017120a908b198a0ffbd7b6c9b
Youssef Lagtab <yousseflagtab@gmail.com>
2024-04-20 17:35:07 +0100

    initial commit

---
+++ src/main/java/com/example/kanflow/KanflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.kanflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kanflow/KanflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kanflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
