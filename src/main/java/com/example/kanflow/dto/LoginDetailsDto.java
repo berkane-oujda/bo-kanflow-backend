a073249354ae74c0f3633c494ae22f7595c6065b
Youssef Lagtab <yousseflagtab@gmail.com>
2024-04-30 07:22:39 +0100

    add dto

---
+++ src/main/java/com/example/kanflow/dto/LoginDetailsDto.java
@@ -0,0 +1,28 @@
+package com.example.kanflow.dto;
+
+public class LoginDetailsDto {
+
+    String email;
+    String password;
+
+    public LoginDetailsDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
