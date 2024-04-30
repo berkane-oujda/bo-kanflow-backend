b57953f42574f62c3c5be1ae995161cfd5ed83a2
Youssef Lagtab <yousseflagtab@gmail.com>
2024-04-30 07:23:10 +0100

    add TokenService

---
+++ src/main/java/com/example/kanflow/service/TokenService.java
@@ -0,0 +1,33 @@
+package com.example.kanflow.service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import com.example.kanflow.model.User;
+
+@Service
+public class TokenService {
+
+    private final JwtEncoder encoder;
+
+    public TokenService(JwtEncoder encoder) {
+        this.encoder = encoder;
+    }
+
+    public String generateToken(User user) {
+        Instant now = Instant.now();
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(15, ChronoUnit.DAYS))
+                .subject("kanflow")
+                .build();
+        return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+}
