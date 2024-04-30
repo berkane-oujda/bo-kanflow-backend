e9d0f0229162d90df64bfa6674f21cd68312f8d9
Youssef Lagtab <yousseflagtab@gmail.com>
2024-04-30 09:58:15 +0100

    clean main app

--- src/main/java/com/example/kanflow/KanflowApplication.java
+++ src/main/java/com/example/kanflow/KanflowApplication.java
@@ -2,20 +2,16 @@ package com.example.kanflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.example.kanflow.config.RsaKeyProperties;
 
 @SpringBootApplication
-@RestController
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class KanflowApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KanflowApplication.class, args);
-        System.out.println("First Run");
     }
 
-    @GetMapping("/status")
-    public String hello() {
-        return "Server is up and running";
-    }
 }
