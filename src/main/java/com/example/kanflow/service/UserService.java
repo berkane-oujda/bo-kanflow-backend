33a6bcbb16c1d1b685847ed38bd7f575b11c5493
Youssef Lagtab <yousseflagtab@gmail.com>
2024-07-03 02:19:00 +0100

    add services

---
+++ src/main/java/com/example/kanflow/service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.kanflow.service;
+import java.util.List;
+
+import com.example.kanflow.model.User;
+
+
+
+public interface UserService {
+    User getUserById(Long id);
+    User findByEmail(String email);
+    List<User> getAllUsers();
+    User saveUser(User user);
+    void deleteUser(Long id);
+
+}
